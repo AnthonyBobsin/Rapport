8babb2e76504dc7bc4bceb04715c74816a30a37c
AnthonyBobsin <bobsinj@gmail.com>
2015-08-16 16:57:01 -0400

    Saves messages to mongo

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+mongoose.connect('mongodb://localhost:27017/rapport')
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html')
+  res.sendFile(__dirname + '/dist/index.html')
 })
+
+var MessageSchema = new Schema({
+  text: { type: String }
+})
+var MessageModel = mongoose.model('message', MessageSchema)
+MessageModel.remove().exec() // For now clear messages db on server load.
 
 io.on('connection', function(socket) {
   console.log("a user connected")
@@ -12,7 +28,12 @@ io.on('connection', function(socket) {
     console.log("user disconnected")
   })
   socket.on('chat message', function(msg) {
-    io.emit('chat message', msg)
+    var message = new MessageModel()
+    message.text = msg
+    message.save(function(err) {
+      if (err) console.log(err)
+      io.emit('chat message', msg)
+    })
   })
 })
 
