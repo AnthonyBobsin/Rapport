9b8d6cfa61e8773e455917ece835a155056906eb
AnthonyBobsin <bobsinj@gmail.com>
2015-08-16 21:24:58 -0400

    Heavy folder restructure

--- index.js
+++ index.js
@@ -1,42 +1,35 @@
-var app = require('express')()
-var server = app.listen(3000)
-var io = require('socket.io').listen(server)
+var express = require('express')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
-var messagesController = require('./server/controllers/messagesController')
+var browserify = require('browserify')
+var React = require('react/addons')
+var jsx = require('node-jsx')
+var messagesController = require(__dirname + '/src/server/controllers/messagesController')
 
+// SETUP
 mongoose.connect('mongodb://localhost:27017/rapport')
-
+jsx.install()
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 app.use(bodyParser.json())
+app.use('/js', express.static(__dirname + '/src/client/js'))
+app.use('/css', express.static(__dirname + '/src/client/css'))
+app.use('/vendor', express.static(__dirname + '/src/vendor'))
 
+// API ROUTES
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/dist/index.html')
+  res.sendFile(__dirname + '/src/client/chat.html')
 })
 
 app.post('/message', function(req, res) {
   messagesController.create(req, res, io)
 })
 
-// TODO: Instead of catching message here, have button submit POST /message request
-// and in controller emit message if saved successfully.
-// io.on('connection', function(socket) {
-//   console.log("a user connected")
-//   socket.on('disconnect', function() {
-//     console.log("user disconnected")
-//   })
-//   socket.on('chat message', function(msg) {
-//     var message = new MessageModel()
-//     message.text = msg
-//     message.save(function(err) {
-//       if (err) console.log(err)
-//       else io.emit('chat message', msg)
-//     })
-//   })
-// })
 
-// http.listen(3000, function() {
-//   console.log('listening on *:3000')
-// })
+http.listen(3000, function() {
+  console.log('I\'m listening on *:3000')
+})
