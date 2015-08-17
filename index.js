2f4cb03d30291aa75b0f8a99cc69a08b136226f0
AnthonyBobsin <bobsinj@gmail.com>
2015-08-16 22:42:31 -0400

    Gets React up and running

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
-var browserify = require('browserify')
-var React = require('react/addons')
-var jsx = require('node-jsx')
 var messagesController = require(__dirname + '/src/server/controllers/messagesController')
 
 // SETUP
 mongoose.connect('mongodb://localhost:27017/rapport')
-jsx.install()
 app.use(bodyParser.urlencoded({
   extended: true
 }))
@@ -19,10 +15,11 @@ app.use(bodyParser.json())
 app.use('/js', express.static(__dirname + '/src/client/js'))
 app.use('/css', express.static(__dirname + '/src/client/css'))
 app.use('/vendor', express.static(__dirname + '/src/vendor'))
+app.use('/build', express.static(__dirname + '/build'))
 
 // API ROUTES
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/src/client/chat.html')
+  res.sendFile(__dirname + '/src/client/index.html')
 })
 
 app.post('/message', function(req, res) {
---
+++ src/app/app.jsx
@@ -0,0 +1,11 @@
+var React = require('react')
+
+App = React.createClass({
+  render: function() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+})
+
+module.exports = App
