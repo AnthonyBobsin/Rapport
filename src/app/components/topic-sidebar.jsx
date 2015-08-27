0d0ccfc255e5f6604f73e6037d180c54e5c8680d
AnthonyBobsin <bobsinj@gmail.com>
2015-08-26 23:44:06 -0400

    Finishes sidebar topic renaming implementation

--- src/app/components/topic-sidebar.jsx
+++ src/app/components/topic-sidebar.jsx
@@ -1,30 +1,60 @@
 var React = require('react')
 
 var Topic = React.createClass({
+  getInitialState: function() {
+    return {
+      isEditing: false,
+      topic: this.props.conversation.topic
+    }
+  },
+
+  componentDidUpdate: function() {
+    if (this.state.isEditing) $('input.topic').focus()
+  },
+
   _handleSwitch: function(e) {
     e.stopPropagation()
     this.props.handleSwitch(this.props.conversation.cid)
   },
 
-  _handleRename: function(e) {
-    e.stopPropagation()
-    // Hide text div and show input text
-    // On enter call handleRename with cid and input value
-    this.props.handleRename(this.props.conversation.cid, "Test")
+  triggerRename: function() {
+    this.props.handleRename(this.props.conversation.cid, this.state.topic)
+  },
+
+  startEditing: function(e) {
+    this.setState({isEditing: true})
+  },
+
+  stopEditing: function(e) {
+    this.setState({isEditing: false})
+    this.triggerRename()
+  },
+
+  checkForEnter: function(e) {
+    var keyCode = e.keyCode || e.which
+    if (keyCode == 13) this.stopEditing()
+  },
+
+  updateTopic: function(e) {
+    this.setState({topic: e.target.value})
   },
 
   render: function() {
     var containerDivClassString = "topic-container " + (this.props.conversation.isActive ? "active" : "")
     var statusClassString = "topic-status " + (this.props.conversation.isActive ? "active" : "")
+    var topicTextClassString = "topic " + (this.state.isEditing ? "hide" : "")
+    var topicInputClassString = "topic " + (this.state.isEditing ? "" : "hide")
     var editTopicClassString = "fa fa-pencil " + (this.props.conversation.isActive ? "" : "hide")
 
     return (
       <div className={containerDivClassString}>
         <div className={statusClassString}></div>
-        <div onClick={this._handleSwitch} className="topic">
-          {this.props.conversation.topic}
+        <div onClick={this._handleSwitch} className={topicTextClassString}>
+          {this.state.topic}
         </div>
-        <i onClick={this._handleRename} className={editTopicClassString}></i>
+        <input type="text" onChange={this.updateTopic} onBlur={this.stopEditing} onKeyPress={this.checkForEnter}
+          value={this.state.topic} className={topicInputClassString} />
+        <i onClick={this.startEditing} className={editTopicClassString}></i>
       </div>
       )
   }
@@ -36,8 +66,7 @@ var Sidebar = React.createClass({
       return (
         <Topic handleSwitch={this.props.handleSwitch}
           handleRename={this.props.handleRename}
-          key={i} conversation={conversation}
-        />
+          key={i} conversation={conversation} />
         )
     }, this)
 
