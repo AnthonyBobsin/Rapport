dd8290adcb03e6e5f908f65321d25d9f69040ad0
AnthonyBobsin <bobsinj@gmail.com>
2015-10-31 17:18:57 -0400

    Includes gitignore to ignore node_modules to prevent conflicting node version downloads.

--- src/app/main.js
+++
@@ -1,8 +0,0 @@
-var React = require('react')
-// Here we put our React instance to the global scope. Make sure you do not put it
-// into production and make sure that you close and open your console if the
-// DEV-TOOLS does not display
-window.React = React;
-
-var App = require('./app.jsx')
-React.render(<App/>, document.querySelector('#appContent'))
--- src/server/models/message.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-
-var MessageSchema = new mongoose.Schema({
-  text: { type: String },
-  user: { type: String }
-})
-var Message = mongoose.model('message', MessageSchema)
-
-module.exports = Message
